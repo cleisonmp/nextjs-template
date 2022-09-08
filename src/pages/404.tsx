4d179a8722a16e142b1c89b2256e1cc5a269e13e
cleisonmp <cleison.mp@gmail.com>
2022-09-07 21:05:53 -0300

    feat: add 404 image :lipstick:

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,8 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import Link from 'next/link'
+
+import styles from '../styles/404.module.css'
 
 export default function Custom404() {
   return (
@@ -7,14 +10,15 @@ export default function Custom404() {
       <Head>
         <title>404 Not Found</title>
       </Head>
-      <main className="">
-        <div className="">
-          <h1 className="">
+      <main className={styles.main}>
+        <Image src='/404.svg' width={600} height={450} alt='' />
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-gray-300 font-bold text-2xl'>
             Ooops! The page you&apos;re looking for was not found
           </h1>
-          <Link href="/">
-            <div className="">
-              <a className="">Return Home</a>
+          <Link href='/'>
+            <div className='group bg-gray-800 text-center rounded-full p-4 w-1/2 mx-auto text-lg font-bold cursor-pointer hover:brightness-110 transition-all'>
+              <a className='group-hover:underline'>Return Home</a>
             </div>
           </Link>
         </div>

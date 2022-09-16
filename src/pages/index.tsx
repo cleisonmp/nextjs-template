e0487e9b0e98455b5a3b6d868ae9748d9f08762a
cleisonmp <cleison.mp@gmail.com>
2022-09-16 15:55:44 -0300

    feat: add Seo component :mag:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,21 @@ import { NextPageWithLayout } from './page'
 import { PrimaryLayout } from '../components/layouts/primary/'
 import { SampleCard } from '../components/common/cards/sample'
 import { mockSampleCardProps } from '../components/common/cards/sample/SampleCard.mocks'
+import { Seo } from '../components/common/Seo'
 
 const Home: NextPageWithLayout = () => {
   return (
-    <main className='flex flex-1 flex-col w-full p-4 gap-4'>
-      <h1 className='text-gray-900'>Next.js template</h1>
+    <>
+      <Seo />
+      <main className='flex flex-1 flex-col w-full p-4 gap-4'>
+        <h1 className='text-gray-900'>Next.js template</h1>
 
-      <div className='grid grid-cols-2 max-w-fit gap-10'>
-        <SampleCard {...mockSampleCardProps.base} />
-        <SampleCard {...mockSampleCardProps.base} />
-      </div>
-    </main>
+        <div className='grid grid-cols-2 max-w-fit gap-10'>
+          <SampleCard {...mockSampleCardProps.base} />
+          <SampleCard {...mockSampleCardProps.base} />
+        </div>
+      </main>
+    </>
   )
 }
 

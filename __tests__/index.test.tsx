b83dd9778c44473658ce85ae03696a7402d6b153
cleisonmp <cleison.mp@gmail.com>
2022-09-16 15:55:05 -0300

    build: add router provider to testing :test_tube:

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,10 +1,16 @@
+import { RouterContext } from 'next/dist/shared/lib/router-context'
 import { render, screen } from '@testing-library/react'
 import Home from '../src/pages'
-//import userEvent from '@testing-library/user-event'
+import { createMockRouter } from '../src/lib/utils/testUtils/createMockRouter'
 
+//import userEvent from '@testing-library/user-event'
 describe('Home', () => {
   it('should render a heading', () => {
-    render(<Home />)
+    render(
+      <RouterContext.Provider value={createMockRouter({})}>
+        <Home />
+      </RouterContext.Provider>,
+    )
 
     const heading = screen.getByRole('heading', {
       name: 'Next.js template',
